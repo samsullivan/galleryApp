2b02db0c761510ec4eaa1a242b94f81e2d48d794
Sam Sullivan <samsullivan09@gmail.com>
2017-02-16 18:21:07 -0600

    Modal for full image on click.

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,23 +1,68 @@
 import React from 'react';
+import ReactModal from 'react-modal';
 
 class Thumbnail extends React.Component {
-  render() {
-    let image = this.props.image;
+  constructor(props) {
+    super(props);
+    this.state = {selectedImage: null};
 
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.selectImage(this.props.image);
+  }
+
+  render() {
     return (
-      <img src={image.thumbnailUrl} alt={image.name} />
+      <a onClick={this.handleClick}>
+        <img src={this.props.image.thumbnailUrl} alt={this.props.image.name} />
+      </a>
     );
   }
 }
 
+class FullImage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.image.title}</h1>
+        <img src={this.props.image.url} alt={this.props.image.name} />
+      </div>
+    );
+  }
+}
 
 class Gallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {selectedImage: null};
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal(image) {
+    this.setState({selectedImage: image});
+  }
+
+  closeModal() {
+    this.setState({selectedImage: null});
+  }
+
   render() {
-    let images = this.props.images;
-    
     return (
       <div className="Gallery">
-        {images.map((image) => <Thumbnail image={image} key={image.id} />)}
+        {this.props.images.map((image) =>
+          <Thumbnail selectImage={this.openModal} image={image} key={image.id} />
+        )}
+        <ReactModal
+          isOpen={!!this.state.selectedImage}
+          onRequestClose={this.closeModal}
+          contentLabel="Image"
+        >
+          <FullImage image={this.state.selectedImage} />
+        </ReactModal>
       </div>
     );
   }
